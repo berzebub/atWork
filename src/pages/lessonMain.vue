f054994ba6338d7915a4a1cde6f4c7f81de32fe0
XiaoJ <jmaibok.winner@gmail.com>
2020-06-16 20:24:19 +0700

    LESSON

--- src/pages/lessonMain.vue
+++ src/pages/lessonMain.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>Lesson Main</div>
+  <div>
+    Lesson Main
+    <!-- หน้าแสดงข้อมูล -->
+    <div>
+      <div>
+        <q-btn round color="primary" icon="fas fa-plus" />
+      </div>
+    </div>
+
+    <!-- เพิ่ม Lesson -->
+    <div class="q-pt-md">
+      <span>ชื่อบทเรียน</span>
+      <q-input outlined v-model="text" />
+    </div>
+    <div class="q-pt-md">
+      <span>ชื่อบทเรียน</span>
+      <q-select outlined v-model="model" :options="options" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: "",
+      model: 1,
+      options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
