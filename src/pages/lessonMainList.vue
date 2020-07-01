9241e565dc4f9afbb416bb032bb0197466f850d8
Parichat <parichat06272@gmail.com>
2020-07-01 21:01:51 +0700

    update lesson

--- src/pages/lessonMainList.vue
+++ src/pages/lessonMainList.vue
@@ -13,24 +13,33 @@
         />
       </div>
       <!-- list -->
-      <div class="shadow-1 bg-blue-grey-10" v-for="(item,index) in dataPosition " :key="index">
-        <q-list class="rounded-borders">
+      <div
+        class="shadow-1 bg-blue-grey-10"
+        style="border-radius: 10px"
+        v-for="(item,index) in dataPosition "
+        :key="index"
+      >
+        <q-list class="rounded-borders q-mt-sm box-main" style="border-radius: 10px">
           <q-expansion-item
             group="level"
             @click="showLesson(item.levelId)"
-            style="border-radius: 5px"
             class="text-white text-subtitle1"
             :label="item.name"
           >
-            <q-card>
-              <div class="q-px-md q-py-sm relative-position cursor-pointer" v-ripple>
-                <div class="row">
+            <q-card style="border-bottom-left-radius: 10px ; border-bottom-right-radius:10px">
+              <div class="relative-position cursor-pointer" v-ripple>
+                <div class="row q-px-md">
                   <div class="col-10">
-                    <span class="text-black text-subtitle1">การใช้งานตำแหน่ง</span>
+                    <span class="text-black text-subtitle1">การใช้งาน</span>
 
-                    <q-toggle v-model="status" color="secondary" />
+                    <q-toggle
+                      class="text-blue-grey-10"
+                      @input="statusLevel(item)"
+                      v-model="item.status"
+                      color="secondary"
+                    />
                   </div>
-                  <div class="col" align="right">
+                  <div class="col self-center" align="right">
                     <q-btn
                       size="13px"
                       icon="fas fa-ellipsis-v"
@@ -40,9 +49,9 @@
                       flat
                     >
                       <q-menu>
-                        <q-list style="min-width: 170px">
+                        <q-list style="min-width: 144px">
                           <q-item clickable v-close-popup>
-                            <q-item-section @click="editPositionBtn(item)">แก้ไขระดับการเรียน</q-item-section>
+                            <q-item-section @click="editPositionBtn(item)">แก้ไขตำแหน่ง</q-item-section>
                           </q-item>
                           <q-item clickable v-close-popup>
                             <q-item-section @click="editLessonBtn(item)">แก้ไขบทเรียน</q-item-section>
@@ -55,10 +64,10 @@
                     </q-btn>
                   </div>
                 </div>
-                <q-separator />
+                <q-separator class="bg-blue-grey-10" />
                 <!-- ปุ่มเพิ่มบทเรียน -->
 
-                <div align="center" class="q-py-lg">
+                <div v-show="showLessonList == 0 " align="center" class="q-py-lg">
                   <div @click="addLesson(item) " class="text-blue-grey-10">
                     <u>เพิ่มบทเรียน</u>
                   </div>
@@ -67,10 +76,10 @@
                 <div class="row" v-for="(item2,index2) in showLessonList " :key="index2">
                   <q-separator />
                   <div
-                    class="bg-white text-blue-grey-10 q-pa-md text-subtitle1 col-9"
+                    class="text-blue-grey-10 q-pa-md text-subtitle1 col-9"
                   >{{item2.order}} - {{item2.name}}</div>
 
-                  <div v-show="item2.status != true" class="col q-py-md bg-white" align="right">
+                  <div v-show="item2.status != true" class="col q-py-md" align="right">
                     <q-icon size="16px" name="fas fa-power-off" dense color="negative" flat></q-icon>
                   </div>
 
@@ -82,6 +91,25 @@
         </q-list>
       </div>
     </div>
+
+    <!-- dialog การไม่ให้ลบ -->
+    <q-dialog v-model="dialogNoDelete">
+      <div
+        class="bg-white row justify-center items-center q-py-md"
+        style="width:323px"
+        align="center"
+      >
+        <div class="text-subtitle1 q-mt-md col-12">
+          ต้องการลบบทเรียนภายในบทเรียน
+          <br />
+          "{{namePosition}}"
+        </div>
+
+        <div class="q-pt-lg q-pb-md">
+          <q-btn dense color="blue-grey-10" style="width:120px" @click="okDelete()" label="ตกลง"></q-btn>
+        </div>
+      </div>
+    </q-dialog>
     <!-- dialog ยืนยันการลบ -->
     <q-dialog v-model="dialogDelete">
       <div
@@ -90,34 +118,35 @@
         align="center"
       >
         <div class="text-subtitle1 q-mt-md">
-          กรูณาพิมพ์ "ดำเนินการ "
-          <br />เพื่อลบตำแหน่ง
+          คุณต้องการลบตำแหน่ง
+          <br />
+          "{{namePosition}}"
         </div>
-        <div style="width:300px">
-          <q-input dense outlined v-model="deleteDialog"></q-input>
+
+        <div class="col-6 q-pr-sm" align="right">
+          <q-btn @click="cancelDelete()" dense style="width:120px" outline label="ยกเลิก"></q-btn>
         </div>
-        <div class="col-6">
-          <q-btn dense style="width:120px" outline label="ยกเลิก"></q-btn>
-        </div>
-        <div class="col-6">
-          <q-btn dense color="blue-grey-10" style="width:120px" label="ยืนยัน"></q-btn>
+        <div class="col-6 q-pl-sm" align="left">
+          <q-btn
+            @click="deletePosition()"
+            dense
+            color="blue-grey-10"
+            style="width:120px"
+            label="ยืนยัน"
+          ></q-btn>
         </div>
       </div>
     </q-dialog>
-    <!-- dialog ไม่สามารถลบได้ -->
-    <q-dialog v-model="dialogDelete2">
+    <!-- dialog ลบสำเร็จ -->
+    <q-dialog v-model="deleted">
       <div
         class="bg-white row justify-center items-center"
-        style="width:323px;height:200px"
+        style="width:320px;height:200px"
         align="center"
       >
-        <div class="text-subtitle1 q-mt-md">
-          ไม่สามารถลบตำแหน่งนี้ได้ มีแบบฝึกหัดใช้งาน
-          <br />ตำแหน่ง "xxxxxxxx" อยู่
-        </div>
-
-        <div class="q-mb-md">
-          <q-btn dense color="blue-grey-10" style="width:120px" label="ตกลง"></q-btn>
+        <div>
+          <q-icon name="far fa-check-circle" class="text-secondary" size="40px" />
+          <div class="text-subtitle1 q-pt-md">ลบข้อมูลเรียบร้อยแล้ว</div>
         </div>
       </div>
     </q-dialog>
@@ -134,20 +163,25 @@ export default {
       options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
       status: false,
       deleteDialog: "",
+      dialogNoDelete: false,
       dialogDelete: false,
-      dialogDelete2: false,
 
       dataPosition: [],
       lessonList: "",
       showLessonList: "",
-      isSnap: ""
+      isSnap: "",
+      namePosition: "",
+      dataDelete: "",
+      deleted: false
     };
   },
   methods: {
     addPosition() {
       this.$router.push("lessonInput");
     },
-    editPositionBtn() {},
+    editPositionBtn(levelId) {
+      this.$router.push("lessonEdit/" + levelId.levelId);
+    },
     addLesson(levelId) {
       this.$router.push("lessonUnitList/" + levelId.levelId);
     },
@@ -155,10 +189,28 @@ export default {
       this.$router.push("lessonUnitlist/" + item.levelId);
     },
 
-    deletePositionBtn() {},
+    deletePositionBtn(data) {
+      this.namePosition = data.name;
+      console.log(data);
+
+      db.collection("practice_server")
+        .where("levelId", "==", data.levelId)
+        .get()
+        .then(doc => {
+          if (doc.size > 0) {
+            //  กรณีมีข้อมูลในแบบฝึกหัด ขึ้นแจ้งเตือน ให้ลบ
+            this.dialogNoDelete = true;
+          } else {
+            //  กรณีไม่มีข้อมูลในแบบฝึกหัด ขึ้นแจ้งเตือน ไม่ให้ลบ
+            this.dataDelete = data;
+            this.dialogDelete = true;
+          }
+        });
+    },
 
     loadDataPosition() {
       db.collection("level").onSnapshot(doc => {
+        this.dataPosition = [];
         doc.forEach(element => {
           let showData = {
             levelId: element.id,
@@ -170,6 +222,7 @@ export default {
             return a.name < b.name ? -1 : 1;
           });
         });
+
         this.loadDataLesson();
       });
     },
@@ -193,13 +246,38 @@ export default {
           temp.sort((a, b) => {
             return a.order - b.order;
           });
-          console.log(temp);
+
           this.lessonList = temp;
         });
     },
     showLesson(levelId) {
-      console.log(levelId);
       this.showLessonList = this.lessonList.filter(x => x.levelId == levelId);
+    },
+    okDelete() {
+      this.dialogNoDelete = false;
+    },
+    cancelDelete() {
+      this.dialogDelete = false;
+    },
+    deletePosition() {
+      console.log(this.dataDelete.levelId);
+      db.collection("level")
+        .doc(this.dataDelete.levelId)
+        .delete()
+        .then(() => {
+          this.dialogDelete = false;
+          this.deleted = true;
+          setTimeout(() => {
+            this.deleted = false;
+          }, 1000);
+        });
+    },
+    statusLevel(data) {
+      db.collection("level")
+        .doc(data.levelId)
+        .update({
+          status: data.status
+        });
     }
   },
   mounted() {
@@ -210,5 +288,8 @@ export default {
 </script>
 
 
-<style lang="scss" scoped>
+<style >
+.box-main {
+  border: 1px solid #263238;
+}
 </style>
