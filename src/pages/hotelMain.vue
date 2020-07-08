e2902c404d70a6fa562aa791abbca447cbf0882f
Parichat <parichat06272@gmail.com>
2020-07-08 22:28:24 +0700

    update user

--- src/pages/hotelMain.vue
+++ src/pages/hotelMain.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>เพิ่มกิจการ</div>
+  <q-page>
+    <div align="center">
+      <q-btn label="เพิ่มกิจการ" color="blue-grey-10" style="width:190px"></q-btn>
+    </div>
+    <div>
+      <q-toolbar>
+        <q-toolbar-title class="text-subtitle1">
+          <span>xxxxx</span>
+        </q-toolbar-title>
+      </q-toolbar>
+    </div>
+  </q-page>
 </template>
 
 <script>
